4af2578a528a0b61c69b8f6b120a3b03bcdcf2b0
Artem Slonchynskyi <slonartem24@gmail.com>
2026-02-11 20:10:59 +0200

    template header footer and api done

---
+++ lib/api.ts
@@ -0,0 +1,46 @@
+import type { Note } from "../types/note";
+import axios from "axios";
+export interface FetchNotesResponse {
+    notes: Note[];
+    totalPages: number;
+}
+
+interface FetchNotesParams {
+  page: number;
+  perPage: number;
+  search?: string;
+}
+
+interface CreateNotePayload {
+  title: string;
+  content: string;
+  tag: string;
+}
+
+const api = axios.create({
+  baseURL: 'https://notehub-public.goit.study/api',
+  headers: {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+  },
+});
+
+export const fetchNotes = async (
+  params: FetchNotesParams
+): Promise<FetchNotesResponse> => {
+  const response = await api.get<FetchNotesResponse>('/notes', {
+    params,
+  });
+  return response.data;
+};
+
+export const createNote = async (
+  payload: CreateNotePayload
+): Promise<Note> => {
+  const response = await api.post<Note>('/notes', payload);
+  return response.data;
+};
+
+export const deleteNote = async (id: string): Promise<Note> => {
+  const response = await api.delete<Note>(`/notes/${id}`);
+  return response.data;
+};
