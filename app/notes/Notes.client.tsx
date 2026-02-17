ea025dd132a175a1e55649f37be1bfbca0fc3e4e
Artem Slonchynskyi <slonartem24@gmail.com>
2026-02-17 18:24:36 +0200

    modal and searchbox done

--- app/notes/Notes.client.tsx
+++ app/notes/Notes.client.tsx
@@ -1,13 +1,18 @@
 "use client";
-
+import css from "./NotesPage.module.css"
 import { useQuery } from "@tanstack/react-query";
 import { fetchNotes } from "@/lib/api";
 import NoteList from "@/components/NoteList/NoteList";
+import { useState } from "react";
+import Modal from "@/components/Modal/Modal";
+import NoteForm from "@/components/NoteForm/NoteForm";
+import SearchBox from "@/components/SearchBox/SearchBox";
 
 export default function NotesClient() {
-  const page = 1;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
   const perPage = 12;
-  const search = "";
 
   const { data, isLoading, error } = useQuery({
     queryKey: ["notes", { page, perPage, search }],
@@ -25,5 +30,23 @@ export default function NotesClient() {
   if (error) return <p>Something went wrong.</p>;
   if (!data) return <p>Something went wrong.</p>;
 
-  return <NoteList notes={data.notes} />;
+  return (
+    <div className={css.app}>
+      <div className={css.toolbar}>
+        <SearchBox value={search} onChange={setSearch} />
+        <button
+          className={css.button}
+          onClick={() => setIsModalOpen(true)}
+        >
+          Create note
+        </button>
+      </div>
+      <NoteList notes={data.notes} />
+      {isModalOpen && (
+        <Modal onClose={() => setIsModalOpen(false)}>
+          <NoteForm onCancel={() => setIsModalOpen(false)} />
+        </Modal>
+      )}
+    </div>
+  );
 }
---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+import css from "./Modal.module.css";
+
+interface Props {
+  children: React.ReactNode;
+  onClose: () => void;
+}
+
+export default function Modal({ children, onClose }: Props) {
+  useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <div className={css.backdrop} onClick={onClose}>
+      <div
+        className={css.modal}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>,
+    document.body
+  );
+}
